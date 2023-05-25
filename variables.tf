b4bf57005cc490db75002cce248726a57be76dc0
Tomas Balaz <tomasbalaz3012@gmail.com>
2023-05-25 13:31:31 +0200

    chore: align module to the template repo

--- variables.tf
+++ variables.tf
@@ -34,16 +34,19 @@ variable "image_tag" {
 }
 
 variable "ecs_cluster_name" {
+  type        = string
   default     = ""
   description = "(Optional) Name of the ECS Cluster that you want to execute your cron job. Defaults to your task name if no value is supplied"
 }
 
 variable "task_cpu" {
+  type        = number
   default     = 1024
   description = "CPU units to allocate to your job (vCPUs * 1024)"
 }
 
 variable "task_memory" {
+  type        = number
   default     = 2048
   description = "In MiB"
 }
@@ -73,11 +76,13 @@ variable "extra_container_defs" {
 }
 
 variable "task_role_arn" {
+  type        = string
   default     = null
   description = "IAM role ARN for your task if it needs to access any AWS resources.  IMPORTANT: This must have an AssumeRolePolicy that includes the 'ecs-tasks.amazonaws.com' provider!!"
 }
 
 variable "ecs_task_execution_role_name" {
+  type        = string
   default     = ""
   description = "If the default AWS ECSTaskExecutionRole is not sufficient for your needs, you can provide your own ECS Task Execution Role here. The module will attach a CloudWatch policy for logging purposes."
 }
